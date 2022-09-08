658fe87b3e1733673ab96283835e7ea8cde89e4a
Michael Estrada <dev1@codeskills.com.co>
2022-09-07 19:40:06 -0500

    Se presenta el momento 1

--- src/controllers/controladorCinema.js
+++ src/controllers/controladorCinema.js
@@ -2,92 +2,112 @@ let peliculas = Array(
     {
         nombre:"Aliens Xmas",
         genero:"Ciencia Ficcion",
-        duracion: 220,
+        duracion: 200,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/alien%20x%20mas.webp?alt=media&token=7f90fddf-b489-4be7-8523-b6ae33f54394",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "La trama se centra en X, un extraterrestre perteneciente a una especie de alienígenas ladrones conocidos como Klepts, que es enviado al polo norte en una misión para destruir la gravedad de la Tierra",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Pepito Perez",
+        actores:["juan alvarez", "Michael Estrada","Camilo Valencia"]
     },
     {
         nombre:"Cherry",
-        genero:"Ciencia Ficcion",
-        duracion: 220,
+        genero:"Accion",
+        duracion: 100,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/cherry.webp?alt=media&token=db0746ed-b0e8-4aab-8366-49475286279b",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Un exmédico del ejército sufre un trastorno postraumático después de regresar de Irak. Al tratar de lidiar con la enfermedad, termina convirtiéndose en un ladrón de bancos para apoyar su adicción a las drogas.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Pablo Neruda",
+        actores:["kiko garcia", "Vinicius jr","Camilo Sexto, Toto LA Momposina"]
     },
     {
         nombre:"Harry Potter",
         genero:"Ciencia Ficcion",
-        duracion: 220,
+        duracion: 90,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/harry%20potter.webp?alt=media&token=e95c3323-15a2-4a6c-915c-4970935af0ac",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "El día de su cumpleaños, Harry Potter descubre que es hijo de dos conocidos hechiceros, de los que ha heredado poderes mágicos. Debe asistir a una famosa escuela de magia y hechicería, donde entabla una amistad con dos jóvenes que se convertirán en sus compañeros de aventura. Durante su primer año en Hogwarts, descubre que un malévolo y poderoso mago llamado Voldemort está en busca de una piedra filosofal que alarga la vida de quien la posee.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Max Cooper",
+        actores:["Ernesto Huesos", "Nika Milando","Camilo Valencia"]
     },
     {
         nombre:"Luca",
-        genero:"Ciencia Ficcion",
-        duracion: 220,
+        genero:"Drama",
+        duracion: 95,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/luca.webp?alt=media&token=196029d7-d5d7-472b-9fc7-f43e2845a1da",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Luca es un monstruo marino que vive debajo de un pueblo de la costa italiana. Cuando sale a la superficie, adquiere una apariencia humana, y conoce a un nuevo amigo, igual que él. Juntos emprenden una aventura entre humanos que odian a los monstruos.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Andrea Pirlo",
+        actores:["Bad Bunny", "Ernerto Samper","Aida Merlano","Luis Suarez"]
     },
     {
         nombre:"Mortal Kombat",
         genero:"Ciencia Ficcion",
-        duracion: 220,
+        duracion: 65,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/mortal%20kombat.webp?alt=media&token=45cecb9f-a73e-4913-adcb-742fc0ace804",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Cole Young, el luchador de MMA, acostumbrado a recibir palizas por dinero, desconoce su ascendencia, y tampoco sabe por qué el emperador Shang Tsung de Outworld ha enviado a su mejor guerrero, Sub-Zero, un Cryomancer sobrenatural, para dar caza a Cole. Ante esta situación, Cole teme por la seguridad de su familia y busca a Sonya Blade siguiendo las indicaciones de Jax, un comandante de las Fuerzas Especiales que tiene la misma extraña marca de dragón con la que nació Cole",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Mocho Sanchez",
+        actores:["Jonathan Estrada", "Michael Jordan","Victoria Avellaneda"]
     },
     {
         nombre:"No mires arriba",
-        genero:"Ciencia Ficcion",
-        duracion: 220,
+        genero:"Drama",
+        duracion: 70,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/no%20mires%20arriba.webp?alt=media&token=50c1b6cb-e781-4541-bd20-a21bb135defc",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Dos astrónomos mediocres descubren que, en pocos meses, un meteorito destruirá el planeta Tierra. A partir de ese momento, intentan advertir a la humanidad del peligro que se avecina a través de los medios de comunicación.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Jorge Baron",
+        actores:["Pipe Bueno", "Jessi Uribe","Luisito Comunica"]
     },
     {
         nombre:"Spell",
-        genero:"Ciencia Ficcion",
-        duracion: 220,
+        genero:"Comedia",
+        duracion: 85,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/spell.webp?alt=media&token=b5b35856-7758-4fb1-bc5a-0371862a1413",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Después de sobrevivir a un accidente de avión en la zona rural de los Apalaches, un hombre intenta liberarse de la magia oscura de una mujer y salvar a su familia de un ritual siniestro antes de la salida de la luna de sangre.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Carletto Anchelotti",
+        actores:["Juan Pablo Alvarez", "MCesare Corredor","Juan Diego Alvira"]
     },
     {
         nombre:"Spider Man",
         genero:"Ciencia Ficcion",
-        duracion: 220,
+        duracion: 90,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/spider%20man.webp?alt=media&token=4c2b1e92-42a7-471a-a109-ccd0a45d72cf",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Luego de sufrir la picadura de una araña genéticamente modificada, un estudiante de secundaria tímido y torpe adquiere increíbles capacidades como arácnido. Pronto comprenderá que su misión es utilizarlas para luchar contra el mal y defender a sus vecinos.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Edinson Ned",
+        actores:["Lokillo Florez", "Maritea","Camilo Valencia"] 
     },
     {
         nombre:"Ven a jugar",
-        genero:"Ciencia Ficcion",
-        duracion: 220,
+        genero:"Terror",
+        duracion: 90,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/ven%20a%20jugar.webp?alt=media&token=20161fe7-b15d-46ca-bf84-36d60adeaa50",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Una entidad misteriosa usa el teléfono de un niño para entrar en un mundo y sus padres luchan por salvarlo.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "PJuan Gabriel",
+        actores:["juan alvarez", "Michael Estrada","Camilo Valencia"]
     },
     {
         nombre:"Viuda negra",
         genero:"Ciencia Ficcion",
-        duracion: 220,
+        duracion: 70,
         poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/viuda%20negra.webp?alt=media&token=8894b78f-0473-41ad-aa62-4a2c54c3728e",
-        sinopsis: "rgbhjrgbhjb",
+        sinopsis: "Una peligrosa conspiración, relacionada con su pasado, persigue a Natasha Romanoff, también conocida como Viuda Negra. La agente tendrá que lidiar con las consecuencias de haber sido espía, así como con las relaciones rotas, para sobrevivir.",
         clasificacion: "+18",
-        idioma: "En subtitulado"
+        idioma: "En subtitulado",
+        director: "Farina",
+        actores:["Blessd", "Ryan Castro","El Ferxxo"] 
     },
   
 )
@@ -131,8 +151,24 @@ peliculas.forEach(function(pelicula){
     idioma.textContent = " Idioma: "+pelicula.idioma
     //7. creamos la sinopsis de cada pelicula
     let sinopsis = document.createElement("p")
-    sinopsis.classList.add("d-none")
+    sinopsis.classList.add("text-start")
     sinopsis.textContent = " sinopsis: "+pelicula.sinopsis
+    //8. creamos la clasificacion para cada pelicula
+    let clasificacion = document.createElement("h6")
+    clasificacion.classList.add("text-start")
+    clasificacion.textContent = " Clasificacion: "+pelicula.clasificacion
+    //8. creamos el director para cada pelicula
+    let director = document.createElement("h6")
+    director.classList.add("text-start")
+    director.textContent = " Director: "+pelicula.director
+    //8. creamos los actores para cada pelicula
+    let actores = document.createElement("h6")
+    actores.classList.add("text-start")
+    actores.textContent = " Actores: "+pelicula.actores
+     //8. creamos la duracion para cada pelicula
+     let duracion = document.createElement("h6")
+     duracion.classList.add("text-start")
+     duracion.textContent = " Duracion: "+pelicula.duracion
 
     
 
@@ -141,7 +177,11 @@ peliculas.forEach(function(pelicula){
     tarjeta.appendChild(nombre)
     tarjeta.appendChild(genero)
     tarjeta.appendChild(idioma)
+    tarjeta.appendChild(duracion)
     tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(clasificacion)
+    tarjeta.appendChild(director)
+    tarjeta.appendChild(actores)
     columna.appendChild(tarjeta)
     fila.appendChild(columna)   
 
@@ -150,4 +190,143 @@ peliculas.forEach(function(pelicula){
 //Detectando seleccionde una pelicula
 fila.addEventListener("click",function(){
     alert("Esta seleccionando una pelicula")
+})
+
+
+
+
+
+//PEliculas nuevos estrenos
+
+let peliculasNuevas = Array(
+    {
+        nombre:"Dragon Ball",
+        genero:"Ciencia Ficcion",
+        duracion: 200,
+        poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/dragon%20ball%20super.jfif?alt=media&token=ac3e953a-dd14-4681-8c41-9aa816104cbd",
+        sinopsis: "La malvada organización Red Ribbon Army se reforma con nuevos y más poderosos androides, Gamma 1 y Gamma 2 para buscar venganza.",
+        clasificacion: "+18",
+        idioma: "En subtitulado",
+        director: "Pepito Perez",
+        actores:["juan alvarez", "Michael Estrada","Camilo Valencia"]
+    },
+    {
+        nombre:"Los Descendientes",
+        genero:"Misterio",
+        duracion: 100,
+        poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/los%20descendientes.jpg?alt=media&token=4eed91ca-441e-40c6-ad25-d70b3684dd78",
+        sinopsis: "Un hombre lucha, simultáneamente, con la decisión de desconectar a su esposa en estado de coma y contra la presión de sus parientes para que venda unas tierras vírgenes que poseen en Hawái.",
+        clasificacion: "+18",
+        idioma: "En subtitulado",
+        director: "Pablo Neruda",
+        actores:["kiko garcia", "Vinicius jr","Camilo Sexto, Toto LA Momposina"]
+    },
+    {
+        nombre:"Seventh son",
+        genero:"Accion",
+        duracion: 90,
+        poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/seventh%20son.jpg?alt=media&token=3e3bd1e2-97fe-449a-b738-4210fb77e835",
+        sinopsis: "Apartado bruscamente de su tranquila vida en el campo, un joven héroe se embarca en una atrevida aventura con su experimentado mentor para derrocar a la oscura reina y a su ejército de asesinos sobrenaturales que pujan por aniquilar el reino.",
+        clasificacion: "+18",
+        idioma: "En subtitulado",
+        director: "Max Cooper",
+        actores:["Ernesto Huesos", "Nika Milando","Camilo Valencia"]
+    },
+    {
+        nombre:"Free Guy",
+        genero:"Drama",
+        duracion: 95,
+        poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/free%20guy.jpg?alt=media&token=100f0e1f-a550-43cc-a33c-78e1b9e8e86c",
+        sinopsis: "Un cajero de un banco descubre que en realidad es un personaje sin papel dentro de un brutal videojuego de mundo interactivo.",
+        clasificacion: "+18",
+        idioma: "En subtitulado",
+        director: "Andrea Pirlo",
+        actores:["Bad Bunny", "Ernerto Samper","Aida Merlano","Luis Suarez"]
+    },
+    {
+        nombre:"Venganza Implacable",
+        genero:"Ciencia Ficcion",
+        duracion: 65,
+        poster: "https://firebasestorage.googleapis.com/v0/b/cinema-163ae.appspot.com/o/venganza%20implacable.webp?alt=media&token=016d4a47-f938-40f3-8a06-3e36c558b66a",
+        sinopsis: "Un ladrón de bancos acepta entregar todo lo robado a cambio de una rebaja en la condena. Sin embargo, el FBI trata de culparlo de un crimen que no cometió y el criminal, un antiguo militar, usará todos sus recursos para limpiar su nombre.",
+        clasificacion: "+18",
+        idioma: "En subtitulado",
+        director: "Mocho Sanchez",
+        actores:["Jonathan Estrada", "Michael Jordan","Victoria Avellaneda"]
+    },  
+)
+
+//Recorriendo arreglo en JS
+
+let fila2 = document.getElementById("fila2")
+
+peliculasNuevas.forEach(function(pelicula){
+    //console.log(pelicula)
+    console.log(pelicula.nombre)
+    console.log(pelicula.genero)
+    console.log(pelicula.duracion)
+    console.log(pelicula.poster)
+    console.log(pelicula.sinopsis)
+    console.log(pelicula.clasificacion)
+    console.log(pelicula.idioma)
+
+    //traversing (crear etiquetas de html desde js)
+    //1. creamos una columna para cada pelicula
+    let columna = document.createElement("div")
+    columna.classList.add("col")
+    //2. creamos una tarjeta para cada pelicula
+    let tarjeta = document.createElement("div")
+    tarjeta.classList.add("card","h-100")
+    //3. creamos una foto para cada pelicula
+    let poster = document.createElement("img")
+    poster.classList.add("card-img-top")
+    poster.src = pelicula.poster
+    //4. creamos un nombre para cada pelicula
+    let nombre = document.createElement("h3")
+    nombre.classList.add("card-title","text-center")
+    nombre.textContent = pelicula.nombre
+    //5. creamos el genero de cada pelicula
+    let genero = document.createElement("h6")
+    genero.classList.add("text-start")
+    genero.textContent = " Genero: "+pelicula.genero
+    //6. creamos el idioma de cada pelicula
+    let idioma = document.createElement("h6")
+    idioma.classList.add("text-start")
+    idioma.textContent = " Idioma: "+pelicula.idioma
+    //7. creamos la sinopsis de cada pelicula
+    let sinopsis = document.createElement("p")
+    sinopsis.classList.add("text-start")
+    sinopsis.textContent = " sinopsis: "+pelicula.sinopsis
+    //8. creamos la clasificacion para cada pelicula
+    let clasificacion = document.createElement("h6")
+    clasificacion.classList.add("text-start")
+    clasificacion.textContent = " Clasificacion: "+pelicula.clasificacion
+    //8. creamos el director para cada pelicula
+    let director = document.createElement("h6")
+    director.classList.add("text-start")
+    director.textContent = " Director: "+pelicula.director
+    //8. creamos los actores para cada pelicula
+    let actores = document.createElement("h6")
+    actores.classList.add("text-start")
+    actores.textContent = " Actores: "+pelicula.actores
+     //8. creamos la duracion para cada pelicula
+     let duracion = document.createElement("h6")
+     duracion.classList.add("text-start")
+     duracion.textContent = " Duracion: "+pelicula.duracion
+
+    
+
+    //Padres e Hijos    
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombre)
+    tarjeta.appendChild(genero)
+    tarjeta.appendChild(idioma)
+    tarjeta.appendChild(duracion)
+    tarjeta.appendChild(sinopsis)
+    tarjeta.appendChild(clasificacion)
+    tarjeta.appendChild(director)
+    tarjeta.appendChild(actores)
+    columna.appendChild(tarjeta)
+    fila2.appendChild(columna)   
+
 })
